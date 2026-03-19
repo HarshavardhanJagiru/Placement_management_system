6e51a891617f736793046f750757a87217eee2da
Harsha <Harshavardhan0218@gmail.com>
2026-03-19 17:41:48 +0530

    Fix missing jsonify import in app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash, Response, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, session, flash, Response, send_from_directory, jsonify
 import pymysql
 import random
 import csv

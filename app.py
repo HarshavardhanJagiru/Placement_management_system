072c468e62f66d41740b3d5eaf6f92a102bfb703
HarshavardhanJagiru <harshavardhan.jagiru2005@gmail.com>
2026-03-21 16:51:09 +0530

    Correct typo in projection code comment
    Fix typo in comment for projection code.

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-thsi is my projection code 
 from flask import Flask, render_template, request, redirect, url_for, session, flash, Response, send_from_directory, jsonify
 import pymysql
 import random

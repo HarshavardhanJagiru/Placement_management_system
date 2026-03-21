2c3654a669e89e9e08bc6f625ef5be4d0d375f7f
HarshavardhanJagiru <harshavardhan.jagiru2005@gmail.com>
2026-03-21 11:29:19 +0530

    Refactor database config to use environment variables
    Updated database configuration to use environment variables for sensitive information.

--- app.py
+++ app.py
@@ -23,11 +23,14 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # Database configuration
+import os
+
 db_config = {
-    'host': 'localhost',
-    'user': 'Harsha',
-    'password': 'Harsha0218!',
-    'database': 'placement_db',
+    'host': os.getenv("DB_HOST"),
+    'user': os.getenv("DB_USER"),
+    'password': os.getenv("DB_PASSWORD"),
+    'database': os.getenv("DB_NAME"),
+    'port': int(os.getenv("DB_PORT", 3306)),
     'cursorclass': pymysql.cursors.DictCursor
 }
 
